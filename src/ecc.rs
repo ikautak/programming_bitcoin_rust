ee3655ed03e0b9194ccc7ece3be533da62afa344
Katsuaki Oshio <katsuaki.oshio@gmail.com>
2022-09-23 00:24:55 +0900

    feat: ecc test

--- src/ecc.rs
+++ src/ecc.rs
@@ -131,7 +131,7 @@ where
 #[derive(Debug, Copy, Clone)]
 pub struct Point<T>
 where
-    T: NumAssign + std::fmt::Debug + std::cmp::PartialOrd,
+    T: Add<Output = T> + Sub<Output = T> + Mul<Output = T> + Div<Output = T> + PartialEq,
 {
     pub x: Option<T>,
     pub y: Option<T>,
@@ -141,7 +141,13 @@ where
 
 impl<T> Point<T>
 where
-    T: NumAssign + std::fmt::Debug + std::cmp::PartialOrd + Copy,
+    T: Add<Output = T>
+        + Sub<Output = T>
+        + Mul<Output = T>
+        + Div<Output = T>
+        + PartialEq
+        + std::fmt::Debug
+        + Copy,
 {
     pub fn new(x: Option<T>, y: Option<T>, a: T, b: T) -> Self {
         if x.is_none() && y.is_none() {
@@ -172,7 +178,7 @@ where
 
 impl<T> PartialEq for Point<T>
 where
-    T: NumAssign + std::fmt::Debug + std::cmp::PartialOrd,
+    T: Add<Output = T> + Sub<Output = T> + Mul<Output = T> + Div<Output = T> + PartialEq,
 {
     fn eq(&self, rhs: &Self) -> bool {
         return self.x == rhs.x && self.y == rhs.y && self.a == rhs.a && self.b == rhs.b;
@@ -181,7 +187,13 @@ where
 
 impl<T> Add for Point<T>
 where
-    T: NumAssign + std::fmt::Debug + std::cmp::PartialOrd + Copy,
+    T: Add<Output = T>
+        + Sub<Output = T>
+        + Mul<Output = T>
+        + Div<Output = T>
+        + PartialEq
+        + std::fmt::Debug
+        + Copy,
 {
     type Output = Self;
     fn add(self, rhs: Self) -> Self {
@@ -247,6 +259,38 @@ where
             a: self.a,
             b: self.b,
         }
+    }
+}
+
+/// scalar multiplications
+impl<T, U> Mul<U> for Point<T>
+where
+    T: Add<Output = T>
+        + Sub<Output = T>
+        + Mul<Output = T>
+        + Div<Output = T>
+        + PartialEq
+        + std::fmt::Debug
+        + Copy,
+    U: NumAssign + PartialOrd + Copy,
+{
+    type Output = Point<T>;
+    fn mul(self, coefficient: U) -> Point<T> {
+        let mut coef = coefficient;
+        let mut current = self;
+        let mut result = Point::new(None, None, self.a, self.b);
+
+        let zero = U::zero();
+        let one = U::one();
+        let two = one + one;
+        while coef > zero {
+            if coef % two > zero {
+                result = result + current;
+            }
+            current = current + current;
+            coef = coef / two;
+        }
+        result
     }
 }
 
@@ -367,5 +411,115 @@ mod tests {
         let a = Point::new(Some(-1), Some(1), 5, 7);
         let b = Point::new(Some(18), Some(-77), 5, 7);
         assert_eq!(a + a, b);
+    }
+
+    #[test]
+    fn test_ecc_on_curve() {
+        let prime = 223;
+        let a = FieldElement::new(0, prime);
+        let b = FieldElement::new(7, prime);
+
+        let valid_points = vec![(192, 105), (17, 56), (1, 193)];
+
+        for (x_raw, y_raw) in valid_points {
+            let x = FieldElement::new(x_raw, prime);
+            let y = FieldElement::new(y_raw, prime);
+            let _p = Point::new(Some(x), Some(y), a, b);
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_ecc_on_curve_panic0() {
+        let prime = 223;
+        let a = FieldElement::new(0, prime);
+        let b = FieldElement::new(7, prime);
+        let x = FieldElement::new(200, prime);
+        let y = FieldElement::new(119, prime);
+        let _p = Point::new(Some(x), Some(y), a, b);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_ecc_on_curve_panic1() {
+        let prime = 223;
+        let a = FieldElement::new(0, prime);
+        let b = FieldElement::new(7, prime);
+        let x = FieldElement::new(42, prime);
+        let y = FieldElement::new(99, prime);
+        let _p = Point::new(Some(x), Some(y), a, b);
+    }
+
+    #[test]
+    fn test_ecc_add() {
+        let prime = 223i32;
+        let a = FieldElement::new(0, prime);
+        let b = FieldElement::new(7, prime);
+
+        let additions = vec![
+            // x1, y1, x2, y2, x3, y3
+            (192, 105, 17, 56, 170, 142),
+            (47, 71, 117, 141, 60, 139),
+            (143, 98, 76, 66, 47, 71),
+        ];
+
+        for (x1, y1, x2, y2, x3, y3) in additions {
+            let x1 = FieldElement::new(x1, prime);
+            let y1 = FieldElement::new(y1, prime);
+            let p1 = Point::new(Some(x1), Some(y1), a, b);
+
+            let x2 = FieldElement::new(x2, prime);
+            let y2 = FieldElement::new(y2, prime);
+            let p2 = Point::new(Some(x2), Some(y2), a, b);
+
+            let x3 = FieldElement::new(x3, prime);
+            let y3 = FieldElement::new(y3, prime);
+            let p3 = Point::new(Some(x3), Some(y3), a, b);
+            assert_eq!(p1 + p2, p3);
+        }
+    }
+
+    #[test]
+    fn test_ecc_scalar_mul() {
+        let prime = 223i32;
+        let a = FieldElement::new(0, prime);
+        let b = FieldElement::new(7, prime);
+
+        // (coefficient, x1, y1, x2, y2)
+        let multiplications = vec![
+            (2, 192, 105, 49, 71),
+            (2, 143, 98, 64, 168),
+            (2, 47, 71, 36, 111),
+            (4, 47, 71, 194, 51),
+            (8, 47, 71, 116, 55),
+        ];
+
+        for (s, x1, y1, x2, y2) in multiplications {
+            let x1 = FieldElement::new(x1, prime);
+            let y1 = FieldElement::new(y1, prime);
+            let p1 = Point::new(Some(x1), Some(y1), a, b);
+
+            let x2 = FieldElement::new(x2, prime);
+            let y2 = FieldElement::new(y2, prime);
+            let p2 = Point::new(Some(x2), Some(y2), a, b);
+
+            assert_eq!(p1 * s, p2);
+        }
+    }
+
+    #[test]
+    fn test_ecc_scalar_mul_none() {
+        let prime = 223i32;
+        let a = FieldElement::new(0, prime);
+        let b = FieldElement::new(7, prime);
+        let s = 21;
+
+        let x1 = FieldElement::new(47, prime);
+        let y1 = FieldElement::new(71, prime);
+        let p1 = Point::new(Some(x1), Some(y1), a, b);
+
+        let p2 = Point::new(None, None, a, b);
+
+        assert_eq!(p1 * s, p2);
     }
 }
