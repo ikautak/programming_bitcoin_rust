2843ae81c1be4cfe592b34a4aac1de595f89dabb
Katsuaki Oshio <katsuaki.oshio@gmail.com>
2022-09-25 20:34:41 +0900

    feat: add verify()

--- src/ecc.rs
+++ src/ecc.rs
@@ -300,7 +300,25 @@ static N: Lazy<U512> =
 type S256Field = FieldElement<U512>;
 #[derive(Debug)]
 pub struct S256Point {
-    point: Point<S256Field>,
+    pub point: Point<S256Field>,
+}
+
+pub fn modpow<T>(mut x: T, mut exp: T, m: T) -> T
+where
+    T: Num + PartialOrd + Copy,
+{
+    let one = T::one();
+    let mut result = T::one();
+
+    while exp > T::zero() {
+        if exp % (one + one) == one {
+            result = result * x % m;
+        }
+        x = x * x % m;
+        exp = exp / (one + one);
+    }
+
+    result
 }
 
 impl S256Point {
@@ -320,6 +338,16 @@ impl S256Point {
         Self {
             point: self.point * coef,
         }
+    }
+
+    pub fn verify(&self, z: U512, r: U512, s: U512) -> bool {
+        let n = *N;
+        let s_inv = modpow(s, n - 2, n);
+
+        let u = z * s_inv % n;
+        let v = r * s_inv % n;
+        let total = G.rmul(u).point + self.rmul(v).point;
+        total.x.unwrap().num == r
     }
 }
 
@@ -651,5 +679,55 @@ mod tests {
             let res = G.rmul(secret);
             assert_eq!(res, point);
         }
+    }
+
+    #[test]
+    fn test_s256_verify() {
+        let p = S256Point::new(
+            U512::from_str_radix(
+                "887387e452b8eacc4acfde10d9aaf7f6d9a0f975aabb10d006e4da568744d06c",
+                16,
+            )
+            .unwrap(),
+            U512::from_str_radix(
+                "61de6d95231cd89026e286df3b6ae4a894a3378e393e93a0f45b666329a0ae34",
+                16,
+            )
+            .unwrap(),
+        );
+
+        let z = U512::from_str_radix(
+            "ec208baa0fc1c19f708a9ca96fdeff3ac3f230bb4a7ba4aede4942ad003c0f60",
+            16,
+        )
+        .unwrap();
+        let r = U512::from_str_radix(
+            "ac8d1c87e51d0d441be8b3dd5b05c8795b48875dffe00b7ffcfac23010d3a395",
+            16,
+        )
+        .unwrap();
+        let s = U512::from_str_radix(
+            "68342ceff8935ededd102dd876ffd6ba72d6a427a3edb13d26eb0781cb423c4",
+            16,
+        )
+        .unwrap();
+        assert!(p.verify(z, r, s));
+
+        let z = U512::from_str_radix(
+            "7c076ff316692a3d7eb3c3bb0f8b1488cf72e1afcd929e29307032997a838a3d",
+            16,
+        )
+        .unwrap();
+        let r = U512::from_str_radix(
+            "eff69ef2b1bd93a66ed5219add4fb51e11a840f404876325a1e8ffe0529a2c",
+            16,
+        )
+        .unwrap();
+        let s = U512::from_str_radix(
+            "c7207fee197d27c618aea621406f6bf5ef6fca38681d82b2f06fddbdce6feab6",
+            16,
+        )
+        .unwrap();
+        assert!(p.verify(z, r, s));
     }
 }
